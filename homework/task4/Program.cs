fb3810a1351063b25503694f40e207868ac3e640
Tatyana Sidorova <tasidorowa@gmail.com>
2024-02-23 00:55:06 +0300

    Добавила дз

---
+++ homework/task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+
+int N = 60;
+int N2 = N;
+int prod = 1;
+
+while (N2 > 10)
+{
+    N2 /= 10;
+    prod *= 10;
+}
+
+while (prod >= 1)
+{
+    if (prod != 1) Console.Write(N / prod + ",");
+    else Console.Write(N / prod);
+    N %= prod;
+    prod /= 10;
+}
+
